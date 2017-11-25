4c2801311105f5d7e8c9bfbdb81d57f8fdd9c43b
Ashiquecse41 <riderdragon127@gmail.com>
2017-11-25 08:09:56 +0600

    imageslider

---
+++ ImageSlider/ImageSlider/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ImageSlider.Startup))]
+namespace ImageSlider
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
